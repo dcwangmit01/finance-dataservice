20bf1f16f747c95c192e44f121f877914f1f3148
David Wang <dcwangmit01@gmail.com>
2012-02-19 15:35:43 -0800

    Adding base rails project created with:  rails new . --git --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dataservice::Application.config.secret_token = '4c914e2b2835cbae017a719e47760f9e8da1cfe90c925d585e68dce0ec0306dde3ed8bd8ea5b6173eb5b1d55e827a23237b563e0da727387caefebfaab5212b0'
