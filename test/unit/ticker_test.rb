7816b2e2077a49cd261c52046abcd2a5d13f0010
David Wang <dcwangmit01@gmail.com>
2012-03-08 21:16:18 -0800

    Figured out how to disable automatic database rollback

--- test/unit/ticker_test.rb
+++ test/unit/ticker_test.rb
@@ -2,40 +2,29 @@ require 'test_helper'
 require 'ruby-debug'
 
 class TickerTest < ActiveSupport::TestCase
+  self.use_transactional_fixtures = false
 
+  TICKERS = [:C, :V]
 
   setup do
-    self.use_transactional_fixtures = false
-    logger.info("Disabling Transactions")
   end
-
-  test "Ticker.t2" do
-  end
-
+  
   test "Ticker.new()" do
-    
-
     ActiveRecord::Base.transaction do
-      t1 = Ticker.new()
-      t1.name = 'ibm'
-      t1.security_type = :stock
-      t1.save()
-      assert(t1.save())
-      logger.info(t1)
-
-      t2 = Ticker.find_by_name('ibm')
-      assert_equal(t1.name, t2.name)
-
-      t2.underlying = t1.id
-      t2.save()
-      logger.info(t2)
+      TICKERS.each do |ticker|
+        logger.info(ticker)
+        t1 = Ticker.new()
+        t1.name = ticker
+        t1.security_type = :stock
+        t1.save()
+        t1.underlying = t1.id
+        t1.save()
+      end
     end
-    
   end
   
   def logger
     return Rails.logger
   end
-
 
 end
