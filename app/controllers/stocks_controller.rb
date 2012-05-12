ff3403c273fbf65181a3455a04ec956ac7f42831
David Wang <dcwangmit01@gmail.com>
2012-05-12 13:38:03 -0400

    - Added Dividend controller which scrapes yahoo webpages for dividend data - Removed all references to dave or davidwang, prepping for GitHub - Added some doc notes on how to hit the web api.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,16 +3,21 @@ require 'rubygems'
 class StocksController < ApplicationController
 
   def index
+    
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/index.csv'
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/index.xml'
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/index.json'
+
     stocks = Stock.select('distinct symbol').order('symbol ASC')
     __export(stocks)
   end
 
   def export
 
-    # wget -O - 'http://192.168.3.26:3000/stocks/export.csv?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365'
-    # wget -O - 'http://192.168.3.26:3000/stocks/export.xml?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365'
-    # wget -O - 'http://192.168.3.26:3000/stocks/export.json?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365'
-    # wget -O - 'http://192.168.3.26:3000/stocks/export.xml?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365&date=2012-01-26'
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/export.csv?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365'
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/export.xml?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365'
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/export.json?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365'
+    # wget -O - 'https://dataservice.yourdomain.com/stocks/export.xml?symbol=csco&start_date=2012-01-01&end_date=2012-02-01&limit=365&date=2012-01-26'
 
 
     # Start with a reverse
--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -1,2 +1,121 @@
+require 'dataservice/util'
+require 'dataservice/finance'
+
 class Dividend < ActiveRecord::Base
+
+  PRIME = Util::ETime.new(2001, 1, 1)
+  
+  def Dividend.GetLastRecord(symbol)
+    assert(symbol.kind_of?(String) || symbol.kind_of?(Symbol))
+    assert(symbol.length()>0)
+
+    # Get the latest historical record to see when it was last updated
+    s = Dividend.find(:last, :order => "date ASC", :conditions => { :symbol => symbol })
+    return s # nil is okay
+  end
+
+  def Dividend.UpdateAll()
+    logger.info("UpdateAll Starting")
+    Ticker.all(:order => "symbol ASC", 
+               :conditions => { :symbol_type => 'dividend'}).each do |t|
+      Dividend::Update(t.symbol)
+    end    
+  end
+
+  def Dividend.Update(symbol)
+    assert(symbol.kind_of?(String) || symbol.kind_of?(Symbol))
+    assert(symbol.length()>0)
+
+    logger.info("Dividend.Update Starting "+
+                "symbol=[#{symbol}])");
+
+    # Figure out the next
+    start = nil
+    begin
+      s = Dividend::GetLastRecord(symbol)
+      if (s == nil)
+        # There is not yet history for this dividend, so prime it.
+        logger.debug(PRIME.to_yaml())
+        
+        start = PRIME
+        logger.info("Priming historical update for dividend: " +
+                    "symbol=[#{symbol}]: " +
+                    "lastRecordDate=[nil] " + 
+                    "start=[#{start.toDateStr()}]")
+      else
+        # History does exist
+        start = Util::ETime::FromDate(s.date+1)
+        logger.info("Found last historical update for dividend: " +
+                    "symbol=[#{symbol}]: " +
+                    "start=[#{start.toDateStr()}]")
+      end
+    end
+    
+    stop = Finance::MarketDate::GetLastHistoricalMarketDate()
+    if (start.dateBefore?(stop))
+      logger.info("Starting historical update for dividend: " +
+                  "symbol=[#{symbol}]: " +
+                  "start=[#{start.toDateStr()}] " +
+                  "stop=[#{stop.toDateStr()}]")
+      assert(start.kind_of?(Util::ETime))
+      assert(stop.kind_of?(Util::ETime))
+      assert(!start.dateEqual?(stop))
+      Dividend::FetchAndLoad(symbol, start, stop)
+    else
+      logger.info("Skipping unnecessary historical update for dividend: " +
+                  "symbol=[#{symbol}]: " +
+                  "start=[#{start.toDateStr()}] " +
+                  "stop=[#{stop.toDateStr()}]")
+    end
+
+  end
+
+  
+  def Dividend.FetchAndLoad(symbol, start, stop)
+    assert(symbol.kind_of?(String) || symbol.kind_of?(Symbol))
+    assert(symbol.length()>0)
+    assert(start.kind_of?(Util::ETime))
+    assert(stop.kind_of?(Util::ETime))
+    assert(!start.dateEqual?(stop))
+
+    t = Finance::DEFAULT_DATA_DRIVER.new(symbol)
+    
+    sd = t.getHistoricalDividendAndSplitData(start, stop)
+    assert(sd != nil)
+    assert(sd.length()>0)
+    assert(sd.has_key?(:dividends))
+    assert(sd.has_key?(:splits))
+
+    for d in sd[:dividends]
+      
+    end
+
+    for d in sd
+      assert(d.has_key?(:symbol) && d.length()>0)
+      assert(d.has_key?(:open))
+      assert(d.has_key?(:high))
+      assert(d.has_key?(:low))
+      assert(d.has_key?(:close))
+      assert(d.has_key?(:volume))
+      assert(d.has_key?(:date) && d[:date].kind_of?(Util::ETime))
+
+      s = Dividend.new()
+      assert(s != nil)
+      s.symbol  = d[:symbol]
+      s.open    = d[:open]
+      s.high    = d[:high]
+      s.low     = d[:low]
+      s.close   = d[:close]
+      s.volume  = d[:volume]
+      s.date    = d[:date].toDateStr()
+      s.save()
+    end
+    
+  end
+
+  def logger()
+    return Rails.logger
+  end
+
+
 end
